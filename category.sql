d63a159b3eb0d0f3e2ec54e9f9f7e6de353207d7
acque <rengui.wu@advance.ai>
2020-07-14 22:45:20 +0800

    'update'

---
+++ category.sql
@@ -0,0 +1,27 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2020-07-14 22:44:19
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "category"
+#
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `update_time` bigint(20) unsigned DEFAULT NULL,
+  `deleted` tinyint(3) unsigned DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "category"
+#
+
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'tech',1594736070071,1594736070071,0);
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
---
+++ post.sql
@@ -0,0 +1,31 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2020-07-14 22:44:30
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "post"
+#
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `html` text,
+  `create_time` bigint(20) DEFAULT NULL,
+  `update_time` bigint(20) unsigned DEFAULT NULL,
+  `cate_id` int(11) unsigned DEFAULT NULL,
+  `image_list` text,
+  `deleted` tinyint(3) unsigned DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "post"
+#
+
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,'hello11111111111111',NULL,'<p>hello2222222222222222</p>',1118201674,1594735452960,1,NULL,NULL),(2,'hello',NULL,'<p>hello</p>',1118219666,1118219666,1,NULL,NULL),(3,'hello',NULL,'<p>hello</p>',1118236924,1118236924,1,NULL,NULL),(4,'hello',NULL,'<p>hello</p>',1118256899,1118256899,1,NULL,NULL);
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
